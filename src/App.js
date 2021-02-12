bc59fa3d1060a15cb8f366d9b483dfa823781892
PatrickBoynton <jpboynton3@gmail.com>
2021-02-12 16:45:00 -0500

    Added the menu and order classes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
 import {Component} from "react";
 
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+}
+
+class Order extends Component{
+    constructor(props) {
+        super(props);
+    }
+}
+
+
 class App extends Component {
   render() {
     return (
