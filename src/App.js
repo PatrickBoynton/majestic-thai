32646b05d3e899ac35409a94f96405a417c8941d
PatrickBoynton <jpboynton3@gmail.com>
2021-02-14 01:08:53 -0500

    Mostly working.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import {Component} from "react";
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            menuItem: "",
+        }
+    }
     render() {
         const menuItems = this.props.menu?.map((items, index) => (
-            <li onClick={this.props.click} key={index}>{items.item}: ${items.price}</li>
+            <li key={index}>{items.item}: ${items.price} <button onClick={() => this.props?.click(index, this.props)}>Add to Cart</button></li>
         ));
       return (<ul>
                 {menuItems}
@@ -17,19 +23,23 @@ class Order extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            value: ""
+            value: this.props.quantity,
+            menuItem: "Burger"
         }
+        console.log(this.props)
         this.handleChange = this.handleChange.bind(this);
     }
+    //From the docs.
     handleChange(event) {
-        this.setState({value: event.target.value})
+        // console.log(this.props.item);
+        this.setState({value: event.target.value});
+        console.log(this.props)
     }
     render() {
-        const menu = this.props.menu?.map(menuItem => menuItem);
         const order = this.props.order?.map((items, index) =>
             <li key={index}>
-                <h1>{menu[1].item}</h1>
-                <p>${items.price(menu[1].price, this.state.value).toFixed(2)}</p>
+                <h1>{this.props.item}</h1>
+                <p>${this.props.price * this.state.value}</p>
                 <input type="number" value={this.state.value} onChange={this.handleChange}/>
             </li>);
         return <ul>
@@ -40,6 +50,15 @@ class Order extends Component{
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuItem: "Burger",
+            menuPrice: 12.99,
+            quantity: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
     componentDidMount() {
         const menu = [
@@ -48,21 +67,27 @@ class App extends Component {
             {item: "Cake", price: "2.99"},
             {item: "Steak", price: "18.99"}
         ]
-        const order = [
-            {item: "Burger", quantity: 0, price: (price,  quantity) => (price * quantity)}
-        ]
+        const order = [{item: this.state.menuItem, quantity: 1, price: this.state.menuPrice}]
+
         this.setState({menu, order});
     }
-
-    handleClick() {
-        console.log("Clicked!")
+    handleClick(index, props) {
+        console.log(this.state.menuPrice)
+        this.setState({menuItem: props?.menu[index].item, menuPrice: props?.menu[index].price});
     }
-
     render() {
     return (
         <div className="App">
-            <Menu menu={this.state?.menu}/>
-            <Order click={this.handleClick} order={this.state?.order} menu={this.state?.menu}/>
+            <Menu click={this.handleClick}
+                  menu={this.state?.menu}
+                  item={this.state?.menuItem}/>
+
+            <Order order={this.state?.order}
+                   menu={this.state?.menu}
+                   item={this.state?.menuItem}
+                   price={this.state?.menuPrice}
+                   quantity={this.state?.quantity}
+                   />
         </div>
     );
   }
